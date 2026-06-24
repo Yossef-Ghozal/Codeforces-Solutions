fc803e3ad14f0cd53e6d27dde04aa503b39ac96b
Yossef-Ghozal <yossefghozal999@gmail.com>
2026-06-25 01:36:01 +0300

    Sync Codeforces: 2226B - Everything Everywhere [C++23 (GCC 14-64, msys2)]

---
+++ 2226B_Everything_Everywhere.cpp
@@ -0,0 +1,67 @@
+// THE KING OF ICPC IS COMING
+ 
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+ 
+using namespace std;
+using namespace __gnu_pbds;
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+ 
+#define TEST int t; cin>>t; while(t--)
+#define QTEST int q; cin>>q; while(q--)
+#define ll long long
+#define ld long double
+#define int128 __int128_t
+#define INTMX LONG_LONG_MAX
+#define INTMN LONG_LONG_MIN
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+#define YES cout<<"YES"<<'\n'
+#define N1 cout<<"-1"<<'\n'
+#define NO cout<<"NO"<<'\n'
+#define Yes cout<<"Yes"<<'\n'
+#define No cout<<"No"<<'\n'
+#define pans cout<<ans<<'\n'
+#define FORcin(a) for( auto &i : (a) ) cin>>i
+#define FORE(i, start, n) for(ll i=(start); i<=(n); ++i)
+#define FOR(i, start, n)  for(ll i=(start); i<(n); ++i)
+#define FORD(i, start, n) for(ll i=(start); i>=(n); --i)
+#define REmoving(v) (v).erase(unique((v).begin(), (v).end()), (v).end())
+#define SORT(v) sort((v).begin(), (v).end())
+#define RSORT(v) sort((v).rbegin(), (v).rend())
+#define MAXV(v) *max_element((v).begin(), (v).end())
+#define MINV(v) *min_element((v).begin(), (v).end())
+#define endlt(i,n) (i == n ? '\n' : ' ')
+#define The_king main
+ 
+void fast(){ ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr); }
+void file() {
+#ifndef ONLINE_JUDGE
+    freopen("Input.txt", "r", stdin);
+    freopen("Output.txt", "w", stdout);
+#endif
+}
+ 
+ll gcd(ll a, ll b) {
+    while (b != 0) {
+        ll r = a % b;
+        a = b;
+        b = r;
+    }
+    return a;
+}
+ 
+signed The_king() { fast(); file();
+    TEST{
+        ll n; cin >> n;
+        vector<ll> a(n);
+        FORcin(a);
+ 
+        ll ans=0;
+        FOR( i,1,n ) if( abs( a[i] - a[i-1] ) == gcd( a[i] , a[i-1] )  ) ans++;
+ 
+ 
+        pans;
+    }
+}
