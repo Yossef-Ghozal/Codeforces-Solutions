990abffa16c8378bedd9da80265aeb455a42b1a3
Yossef-Ghozal <yossefghozal999@gmail.com>
2026-06-25 00:43:22 +0300

    Sync Codeforces: 1829D - Gold Rush [C++23 (GCC 14-64, msys2)]

---
+++ 1829D_Gold_Rush.cpp
@@ -0,0 +1,78 @@
+// THE KING OF ICPC IS COMING
+ 
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+ 
+using namespace std;
+using namespace __gnu_pbds;
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+ 
+#define TEST int t; cin>>t; while(t--)
+#define QTEST int q; cin>>q; while(q--)
+#define ll long long
+#define ld long double
+#define int128 __int128_t
+#define INTMX LONG_LONG_MAX
+#define INTMN LONG_LONG_MIN
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+#define YES cout<<"YES"<<'\n'
+#define N1 cout<<"-1"<<'\n'
+#define NO cout<<"NO"<<'\n'
+#define Yes cout<<"Yes"<<'\n'
+#define No cout<<"No"<<'\n'
+#define pans cout<<ans<<'\n'
+#define FORcin(a) for( auto &i : (a) ) cin>>i
+#define FORE(i, start, n) for(ll i=(start); i<=(n); ++i)
+#define FOR(i, start, n)  for(ll i=(start); i<(n); ++i)
+#define FORD(i, start, n) for(ll i=(start); i>=(n); --i)
+#define REmoving(v) (v).erase(unique((v).begin(), (v).end()), (v).end())
+#define SORT(v) sort((v).begin(), (v).end())
+#define RSORT(v) sort((v).rbegin(), (v).rend())
+#define MAXV(v) *max_element((v).begin(), (v).end())
+#define MINV(v) *min_element((v).begin(), (v).end())
+#define endlt(i,n) (i == n ? '\n' : ' ')
+#define The_king main
+ 
+void fast(){ ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr); }
+void file() {
+#ifndef ONLINE_JUDGE
+    freopen("Input.txt", "r", stdin);
+    freopen("Output.txt", "w", stdout);
+#endif
+}
+ 
+const int MAXN = 2e5 + 5;
+vector<int> adj[MAXN];
+bool visited[MAXN];
+ 
+bool dfs(long long current, long long target, vector<long long>& path) {
+    if ( current == target ) return true;
+    if ( current <= 0 || current%3 != 0 ) return false;
+    path.push_back(current);
+ 
+    ll part1 = current/3;
+    ll part2 = part1*2;
+ 
+    if ( dfs(part1, target, path) ) return true;
+    if ( dfs(part2, target, path) ) return true;
+ 
+    path.pop_back();
+    return false;
+}
+ 
+signed The_king() { fast(); file();
+    TEST{
+        ll n,m; cin >> n >> m;
+        if ( n == m ) { YES; continue; }
+ 
+        if ( n%3 == 0 && n > m ) {
+            vector<long long> path;
+            bool check = dfs( n , m , path );
+            if ( check ) YES;
+            else NO;
+        }
+        else NO;
+    }
+}
