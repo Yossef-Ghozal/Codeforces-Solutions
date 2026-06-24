2df7e58bdcbb16f20cd3a4272fccfea498a5bb8e
Yossef-Ghozal <yossefghozal999@gmail.com>
2026-06-25 02:24:43 +0300

    Sync Codeforces: 2162B - Beautiful String [C++23 (GCC 14-64, msys2)]

---
+++ 2162B_Beautiful_String.cpp
@@ -0,0 +1,106 @@
+// THE KING OF ICPC IS COMING
+ 
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+ 
+using namespace std;
+using namespace __gnu_pbds;
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+ 
+#define TEST int t; cin>>t; while(t--)
+#define QTEST int q; cin>>q; while(q--)
+#define ll long long
+#define ld long double
+#define int128 __int128_t
+#define INTMX LONG_LONG_MAX
+#define INTMN LONG_LONG_MIN
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+#define YES cout<<"YES"<<'\n'
+#define N1 cout<<"-1"<<'\n'
+#define NO cout<<"NO"<<'\n'
+#define Yes cout<<"Yes"<<'\n'
+#define No cout<<"No"<<'\n'
+#define pans cout<<ans<<'\n'
+#define FORcin(a) for( auto &i : (a) ) cin>>i
+#define FORE(i, start, n) for(ll i=(start); i<=(n); ++i)
+#define FOR(i, start, n)  for(ll i=(start); i<(n); ++i)
+#define FORD(i, start, n) for(ll i=(start); i>=(n); --i)
+#define REmoving(v) (v).erase(unique((v).begin(), (v).end()), (v).end())
+#define SORT(v) sort((v).begin(), (v).end())
+#define RSORT(v) sort((v).rbegin(), (v).rend())
+#define MAXV(v) *max_element((v).begin(), (v).end())
+#define MINV(v) *min_element((v).begin(), (v).end())
+#define endlt(i,n) (i == n ? '\n' : ' ')
+#define The_king main
+ 
+void fast(){ ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr); }
+void file() {
+#ifndef ONLINE_JUDGE
+    freopen("Input.txt", "r", stdin);
+    freopen("Output.txt", "w", stdout);
+#endif
+}
+ll n;
+ 
+string ss;
+ 
+bool is_palindrome(string s)
+{
+    string t = s;
+    reverse(t.begin(), t.end());
+    return s == t;
+}
+ 
+bool is_sort(string s)
+{
+    for ( int i = 1; i < s.size(); i++ ) {
+        if ( s[i] < s[ i - 1 ] ) return false;
+    }
+    return true;
+}
+vector<ll> idx;
+vector<ll> ans;
+bool check = false;
+ 
+void nah ( ll i  , string x , string p ) {
+    if ( i == n ) {
+        if ( is_palindrome( x ) && is_sort( p ) ) {
+            check = true;
+            ans = idx;
+        }
+        return;
+    }
+    idx.push_back( i+1 );
+    nah ( i+1 , x , p+ss[i] );
+    idx.pop_back();
+    nah( i+1 , x+ss[i], p );
+ 
+}
+ 
+signed The_king() { fast(); file();
+    TEST{
+        cin >> n;
+        cin >> ss;
+ 
+        check = false;
+        idx.clear();
+ 
+        if ( is_palindrome(ss) ) {
+            cout<< 0 << endl << endl;
+            continue;
+        }
+        else {
+            nah( 0 , "" , "" );
+            if ( check ){
+                cout<< ans.size() << endl;
+                for ( ll i = 0; i < ans.size(); i++ ) {
+                    cout << ans[i] << " ";
+                }
+                cout << endl;
+            }
+            else N1;
+        }
+    }
+}
