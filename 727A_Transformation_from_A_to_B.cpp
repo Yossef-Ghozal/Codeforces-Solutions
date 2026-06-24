27f2ab12fbf9fcd4c64130ce8b9b0f653f9f3fd2
Yossef-Ghozal <yossefghozal999@gmail.com>
2026-06-25 00:43:11 +0300

    Sync Codeforces: 727A - Transformation: from A to B [C++23 (GCC 14-64, msys2)]

---
+++ 727A_Transformation_from_A_to_B.cpp
@@ -0,0 +1,75 @@
+// THE KING OF ICPC IS COMING
+ 
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+ 
+using namespace std;
+using namespace __gnu_pbds;
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+ 
+#define TEST int t; cin>>t; while(t--)
+#define QTEST int q; cin>>q; while(q--)
+#define ll long long
+#define ld long double
+#define int128 __int128_t
+#define INTMX LONG_LONG_MAX
+#define INTMN LONG_LONG_MIN
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+#define YES cout<<"YES"<<'\n'
+#define N1 cout<<"-1"<<'\n'
+#define NO cout<<"NO"<<'\n'
+#define Yes cout<<"Yes"<<'\n'
+#define No cout<<"No"<<'\n'
+#define pans cout<<ans<<'\n'
+#define FORcin(a) for( auto &i : (a) ) cin>>i
+#define FORE(i, start, n) for(ll i=(start); i<=(n); ++i)
+#define FOR(i, start, n)  for(ll i=(start); i<(n); ++i)
+#define FORD(i, start, n) for(ll i=(start); i>=(n); --i)
+#define REmoving(v) (v).erase(unique((v).begin(), (v).end()), (v).end())
+#define SORT(v) sort((v).begin(), (v).end())
+#define RSORT(v) sort((v).rbegin(), (v).rend())
+#define MAXV(v) *max_element((v).begin(), (v).end())
+#define MINV(v) *min_element((v).begin(), (v).end())
+#define endlt(i,n) (i == n ? '\n' : ' ')
+#define The_king main
+ 
+void fast(){ ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr); }
+void file() {
+#ifndef ONLINE_JUDGE
+    freopen("Input.txt", "r", stdin);
+    freopen("Output.txt", "w", stdout);
+#endif
+}
+ 
+const int MAXN = 1e5 + 5;
+vector<int> adj[MAXN];
+bool visited[MAXN];
+ 
+bool dfs(long long current, long long target, vector<long long>& path) {
+    if ( current > target ) return false;
+    path.push_back(current);
+ 
+    if ( current == target ) return true;
+    if ( dfs(current*2, target, path) ) return true;
+    if ( dfs(current*10+1, target, path) ) return true;
+ 
+    path.pop_back();
+    return false;
+}
+ 
+signed The_king() { fast(); file();
+    ll n , start; cin >> n >> start;
+    vector<ll> path;
+ 
+    bool check = dfs( n , start , path );
+    if ( check ) {
+        YES;
+        cout<< path.size() <<endl;
+        for ( auto i : path ) {
+            cout<< i <<" ";
+        }
+    }
+    else NO;
+}
