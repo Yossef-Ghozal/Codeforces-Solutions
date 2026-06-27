f0be895be9e8bda80332a74f6461328a1e0b96ff
Yossef-Ghozal <yossefghozal999@gmail.com>
2026-06-27 18:52:58 +0300

    Sync Codeforces: 2240B - AI Finds Nothing Here [C++23 (GCC 14-64, msys2)]

---
+++ 2240B_AI_Finds_Nothing_Here.cpp
@@ -0,0 +1,62 @@
+// THE KING OF ICPC IS COMING
+ 
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+ 
+using namespace std;
+using namespace __gnu_pbds;
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+ 
+#define TEST int t; cin>>t; while(t--)
+#define QTEST int q; cin>>q; while(q--)
+#define ll long long
+#define ld long double
+#define int128 __int128_t
+#define INTMX LONG_LONG_MAX
+#define INTMN LONG_LONG_MIN
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+#define YES cout<<"YES"<<'\n'
+#define N1 cout<<"-1"<<'\n'
+#define NO cout<<"NO"<<'\n'
+#define Yes cout<<"Yes"<<'\n'
+#define No cout<<"No"<<'\n'
+#define pans cout<<ans<<'\n'
+#define FORcin(a) for( auto &i : (a) ) cin>>i
+#define FORE(i, start, n) for(ll i=(start); i<=(n); ++i)
+#define FOR(i, start, n)  for(ll i=(start); i<(n); ++i)
+#define FORD(i, start, n) for(ll i=(start); i>=(n); --i)
+#define REmoving(v) (v).erase(unique((v).begin(), (v).end()), (v).end())
+#define SORT(v) sort((v).begin(), (v).end())
+#define RSORT(v) sort((v).rbegin(), (v).rend())
+#define MAXV(v) *max_element((v).begin(), (v).end())
+#define MINV(v) *min_element((v).begin(), (v).end())
+#define endlt(i,n) (i == n ? '\n' : ' ')
+#define The_king main
+ 
+void fast(){ ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr); }
+void file() {
+#ifndef ONLINE_JUDGE
+    freopen("Input.txt", "r", stdin);
+    freopen("Output.txt", "w", stdout);
+#endif
+}
+ 
+ll fastPowerMod(ll a, ll b, ll mod) {
+    ll res = 1;
+    a %= mod;
+    while (b > 0) {
+        if (b & 1) res = (res * a) % mod;
+        a = (a * a) % mod;
+        b >>= 1;
+    }
+    return res;
+}
+ 
+signed The_king() { fast(); file();
+    TEST{
+        ll n,m,a,b; cin>>n>>m>>a>>b;
+        cout<< fastPowerMod(2, (n*m) - (n-a+1)*(m-b+1) , 998244353)<<endl;
+    }
+}
