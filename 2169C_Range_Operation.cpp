669be7de07a2b7e9acc95a0d37209b5fcc205fe1
Yossef-Ghozal <yossefghozal999@gmail.com>
2026-07-19 18:39:57 +0300

    Sync Codeforces: 2169C - Range Operation [C++23 (GCC 14-64, msys2)]

---
+++ 2169C_Range_Operation.cpp
@@ -0,0 +1,72 @@
+// THE KING OF ICPC IS COMING
+ 
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+ 
+using namespace std;
+using namespace __gnu_pbds;
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+ 
+#define TEST int t; cin>>t; while(t--)
+#define QTEST int q; cin>>q; while(q--)
+#define ll long long
+#define ld long double
+#define int128 __int128_t
+#define INTMX LONG_LONG_MAX
+#define INTMN LONG_LONG_MIN
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+#define YES cout<<"YES"<<'\n'
+#define N1 cout<<"-1"<<'\n'
+#define NO cout<<"NO"<<'\n'
+#define Yes cout<<"Yes"<<'\n'
+#define No cout<<"No"<<'\n'
+#define pans cout<<ans<<'\n'
+#define FORcin(a) for( auto &i : (a) ) cin>>i
+#define FORE(i, start, n) for(ll i=(start); i<=(n); ++i)
+#define FOR(i, start, n)  for(ll i=(start); i<(n); ++i)
+#define FORD(i, start, n) for(ll i=(start); i>=(n); --i)
+#define REmoving(v) (v).erase(unique((v).begin(), (v).end()), (v).end())
+#define SORT(v) sort((v).begin(), (v).end())
+#define RSORT(v) sort((v).rbegin(), (v).rend())
+#define MAXV(v) *max_element((v).begin(), (v).end())
+#define MINV(v) *min_element((v).begin(), (v).end())
+#define endlt(i,n) (i == n ? '\n' : ' ')
+#define dkhan_mn_ghyr_kwfta main
+ 
+void fast(){ ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr); }
+void file() {
+#ifndef ONLINE_JUDGE
+    freopen("Input.txt", "r", stdin);
+    freopen("Output.txt", "w", stdout);
+#endif
+}
+ 
+ll ceildiv(ll num1, ll num2){
+    if(num1 >= 0) return (num1 + num2 - 1) / num2;
+    return - ( (-num1) / num2 );
+}
+ 
+signed dkhan_mn_ghyr_kwfta() { fast(); file();
+    TEST{
+        ll n; cin>>n;
+        vector<ll> a(n);
+        vector<ll> pref(n+1);
+        FORcin(a);
+ 
+        for (ll i = 1; i <= n; i++){
+            pref[i] += a[i-1];
+            if (i) { pref[i] += pref[i - 1]; }
+        }
+ 
+        ll mn = 0 , mx = 0;
+        for ( ll i=1 ; i <= n; i++) {
+            ll x = i*(i+1)-pref[i];
+            mx = max(mx,x-mn );
+            mn = min(mn,x);
+        }
+ 
+        cout<< pref[n] + mx << endl;
+    }
+}
