06edbff70846800bbbb6c1e245240c76a7cae62e
Yossef-Ghozal <yossefghozal999@gmail.com>
2026-07-23 22:30:00 +0300

    Sync Codeforces: 106619L - Appending LIS [C++23 (GCC 14-64, msys2)]

---
+++ 106619L_Appending_LIS.cpp
@@ -0,0 +1,73 @@
+// THE KING OF ICPC IS COMING
+ 
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+ 
+using namespace std;
+using namespace __gnu_pbds;
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+ 
+#define TEST int t; cin>>t; while(t--)
+#define QTEST int q; cin>>q; while(q--)
+#define ll long long
+#define ld long double
+#define int128 __int128_t
+#define INTMX LONG_LONG_MAX
+#define INTMN LONG_LONG_MIN
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+#define YES cout<<"YES"<<'\n'
+#define N1 cout<<"-1"<<'\n'
+#define NO cout<<"NO"<<'\n'
+#define Yes cout<<"Yes"<<'\n'
+#define No cout<<"No"<<'\n'
+#define pans cout<<ans<<'\n'
+#define FORcin(a) for( auto &i : (a) ) cin>>i
+#define FORE(i, start, n) for(ll i=(start); i<=(n); ++i)
+#define FOR(i, start, n)  for(ll i=(start); i<(n); ++i)
+#define FORD(i, start, n) for(ll i=(start); i>=(n); --i)
+#define REmoving(v) (v).erase(unique((v).begin(), (v).end()), (v).end())
+#define SORT(v) sort((v).begin(), (v).end())
+#define RSORT(v) sort((v).rbegin(), (v).rend())
+#define MAXV(v) *max_element((v).begin(), (v).end())
+#define MINV(v) *min_element((v).begin(), (v).end())
+#define endlt(i,n) (i == n ? '\n' : ' ')
+#define dkhan_mn_ghyr_kwfta main
+ 
+void fast(){ ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr); }
+void file() {
+#ifndef ONLINE_JUDGE
+    freopen("Input.txt", "r", stdin);
+    freopen("Output.txt", "w", stdout);
+#endif
+}
+ 
+long long sum_by_a1_d(long long a1, long long d, long long n) {
+    return (2 * a1 + (n - 1) * d) * n / 2;
+}
+ 
+signed dkhan_mn_ghyr_kwfta() { fast(); file();
+    TEST{
+        ll n,k; cin>>n>>k;
+        vector<ll> a(n);
+        for(ll i=0;i<n;i++) cin>>a[i];
+ 
+        ll sum =0;
+        vector<ll> dp;
+ 
+        for ( int i = 0; i < n; i++ ) {
+            sum = sum + a[i];
+ 
+            auto it = lower_bound(dp.begin(), dp.end(), a[i]);
+            if ( it == dp.end() ) dp.push_back(a[i]);
+            else *it = a[i];
+ 
+            ll len = dp.size();
+            if ( len > dp.back() ) cout<< sum + sum_by_a1_d( len , 1, k ) <<" ";
+            else cout<< sum + len * k << " ";
+        }
+ 
+        cout<< endl;
+    }
+}
