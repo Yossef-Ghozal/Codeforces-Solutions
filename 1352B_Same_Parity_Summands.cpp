80d4615928cc2f512a0495ddc422fd921f65520f
Yossef-Ghozal <yossefghozal999@gmail.com>
2026-08-11 18:59:57 +0300

    Sync Codeforces: 1352B - Same Parity Summands [C++23 (GCC 14-64, msys2)]

---
+++ 1352B_Same_Parity_Summands.cpp
@@ -0,0 +1,66 @@
+// THE KING OF ICPC IS COMING
+ 
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+ 
+using namespace std;
+using namespace __gnu_pbds;
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+ 
+#define TEST int t; cin>>t; while(t--)
+#define QTEST int q; cin>>q; while(q--)
+#define ll long long
+#define ld long double
+#define int128 __int128_t
+#define INTMX LONG_LONG_MAX
+#define INTMN LONG_LONG_MIN
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+#define YES cout<<"YES"<<'\n'
+#define N1 cout<<"-1"<<'\n'
+#define NO cout<<"NO"<<'\n'
+#define Yes cout<<"Yes"<<'\n'
+#define No cout<<"No"<<'\n'
+#define pans cout<<ans<<'\n'
+#define FORcin(a) for( auto &i : (a) ) cin>>i
+#define FORE(i, start, n) for(ll i=(start); i<=(n); ++i)
+#define FOR(i, start, n)  for(ll i=(start); i<(n); ++i)
+#define FORD(i, start, n) for(ll i=(start); i>=(n); --i)
+#define REmoving(v) (v).erase(unique((v).begin(), (v).end()), (v).end())
+#define SORT(v) sort((v).begin(), (v).end())
+#define RSORT(v) sort((v).rbegin(), (v).rend())
+#define MAXV(v) *max_element((v).begin(), (v).end())
+#define MINV(v) *min_element((v).begin(), (v).end())
+#define endlt(i,n) (i == n ? '\n' : ' ')
+#define dkhan_mn_ghyr_kwfta main
+ 
+ 
+void fast(){ ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr); }
+void file() {
+#ifndef ONLINE_JUDGE
+    freopen("Input.txt", "r", stdin);
+    freopen("Output.txt", "w", stdout);
+#endif
+}
+ 
+signed dkhan_mn_ghyr_kwfta() { fast(); file();
+    TEST{
+        ll n,k; cin>>n>>k;
+ 
+        if ( (n-(k-1))%2 != 0 and (n-(k-1)) > 0 ) {
+            YES;
+            for ( int i=0 ; i<k-1 ; i++) cout<<1<<" ";
+            cout<< n-(k-1) <<endl;
+            continue;
+        }
+        if ( (n-2*(k-1))%2 == 0 and (n-2*(k-1)) > 0 ) {
+            YES;
+            for ( int i=0 ; i<k-1 ; i++) cout<<2<<" ";
+            cout<< n-2*(k-1) <<endl;
+            continue;
+        }
+        NO;
+    }
+}
