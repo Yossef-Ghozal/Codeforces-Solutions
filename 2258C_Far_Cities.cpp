d7b5e6fd22bb86add201172d38a8834137d7dd10
Yossef-Ghozal <yossefghozal999@gmail.com>
2026-08-29 19:09:01 +0300

    Sync Codeforces: 2258C - Far Cities [C++23 (GCC 14-64, msys2)]

---
+++ 2258C_Far_Cities.cpp
@@ -0,0 +1,98 @@
+// THE KING OF ICPC IS COMING
+ 
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+ 
+using namespace std;
+using namespace __gnu_pbds;
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+ 
+#define TEST int t; cin>>t; while(t--)
+#define QTEST int q; cin>>q; while(q--)
+#define ll long long
+#define ld long double
+#define int128 __int128_t
+#define INTMX LONG_LONG_MAX
+#define INTMN LONG_LONG_MIN
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+#define YES cout<<"YES"<<'\n'
+#define N1 cout<<"-1"<<'\n'
+#define NO cout<<"NO"<<'\n'
+#define Yes cout<<"Yes"<<'\n'
+#define No cout<<"No"<<'\n'
+#define pans cout<<ans<<'\n'
+#define FORcin(a) for( auto &i : (a) ) cin>>i
+#define FORE(i, start, n) for(ll i=(start); i<=(n); ++i)
+#define FOR(i, start, n)  for(ll i=(start); i<(n); ++i)
+#define FORD(i, start, n) for(ll i=(start); i>=(n); --i)
+#define REmoving(v) (v).erase(unique((v).begin(), (v).end()), (v).end())
+#define SORT(v) sort((v).begin(), (v).end())
+#define RSORT(v) sort((v).rbegin(), (v).rend())
+#define MAXV(v) *max_element((v).begin(), (v).end())
+#define MINV(v) *min_element((v).begin(), (v).end())
+#define endlt(i,n) (i == n ? '\n' : ' ')
+#define The_king main
+ 
+void fast(){ ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr); }
+void file() {
+#ifndef ONLINE_JUDGE
+    freopen("Input.txt", "r", stdin);
+    freopen("Output.txt", "w", stdout);
+#endif
+}
+ 
+ll ask( ll d , ll u , ll v ) {
+    cout<< "? " << u <<" "<< v << " " << d << endl;
+    cout.flush();
+    ll res; cin>>res;
+    return res;
+}
+ 
+signed The_king() { fast(); file();
+    TEST{
+        ll n; cin >> n;
+        vector<ll> a(3);
+        a[0]=1; a[1]=2; a[2]=0;
+ 
+        while (a[2] < n) {
+            ll res = ask(a[2] + 1, a[0], a[1]);
+            if (res == -1) return 0;
+            if (res == 1) a[2]++;
+            else break;
+        }
+ 
+           for ( int i = 3 ; i<=n ; i++ ) {
+               ll u = a[0];
+               ll v = a[1];
+ 
+               while (a[2] < n) {
+                   ll res1 = ask(a[2] + 1, u , i);
+                   if (res1 == -1) return 0;
+                   if (res1 == 1) {
+                       a[1] = i;
+                       a[0] = u;
+                       a[2]++;
+                   } else {
+                       break;
+                   }
+               }
+ 
+               while (a[2] < n) {
+                   ll res1 = ask(a[2] + 1, i , v );
+                   if (res1 == -1) return 0;
+                   if (res1 == 1) {
+                       a[0] = i;
+                       a[1] = v;
+                       a[2]++;
+                   } else {
+                       break;
+                   }
+               }
+           }
+ 
+        cout<<"! "<<a[0]<<" "<<a[1]<<" "<<a[2]<<endl;
+        cout.flush();
+    }
+}
