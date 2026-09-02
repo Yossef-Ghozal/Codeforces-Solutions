b975404f39dfa08391109db9381985bbca4c19a8
Yossef-Ghozal <yossefghozal999@gmail.com>
2026-09-02 20:10:57 +0300

    Sync Codeforces: 2231C - Chipmunk Theo and Equality [C++23 (GCC 14-64, msys2)]

---
+++ 2231C_Chipmunk_Theo_and_Equality.cpp
@@ -0,0 +1,95 @@
+// THE KING OF ICPC IS COMING
+ 
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+ 
+using namespace std;
+using namespace __gnu_pbds;
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+ 
+#define TEST int t; cin>>t; while(t--)
+#define QTEST int q; cin>>q; while(q--)
+#define ll long long
+#define ld long double
+#define int128 __int128_t
+#define INTMX LONG_LONG_MAX
+#define INTMN LONG_LONG_MIN
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+#define YES cout<<"YES"<<'\n'
+#define N1 cout<<"-1"<<'\n'
+#define NO cout<<"NO"<<'\n'
+#define Yes cout<<"Yes"<<'\n'
+#define No cout<<"No"<<'\n'
+#define pans cout<<ans<<'\n'
+#define FORcin(a) for( auto &i : (a) ) cin>>i
+#define FORE(i, start, n) for(ll i=(start); i<=(n); ++i)
+#define FOR(i, start, n)  for(ll i=(start); i<(n); ++i)
+#define FORD(i, start, n) for(ll i=(start); i>=(n); --i)
+#define REmoving(v) (v).erase(unique((v).begin(), (v).end()), (v).end())
+#define SORT(v) sort((v).begin(), (v).end())
+#define RSORT(v) sort((v).rbegin(), (v).rend())
+#define MAXV(v) *max_element((v).begin(), (v).end())
+#define MINV(v) *min_element((v).begin(), (v).end())
+#define endlt(i,n) (i == n ? '\n' : ' ')
+#define The_king main
+ 
+void fast(){ ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr); }
+void file() {
+#ifndef ONLINE_JUDGE
+    freopen("Input.txt", "r", stdin);
+    freopen("Output.txt", "w", stdout);
+#endif
+}
+ 
+signed The_king() { fast(); file();
+    TEST{
+        ll n; cin>>n;
+        vector<ll> a(n);
+        for(ll i=0;i<n;i++) cin>>a[i];
+ 
+        vector<pair<int , int >> vals;
+        vals.reserve(n * 65);
+ 
+        for ( int i=0;i<n;i++) {
+            ll x = a[i];
+            ll proc = 0;
+            while ( x >= 1 ) {
+                vals.push_back({x, proc});
+ 
+                if (x == 1) {
+                    if (proc == 0) {
+                        vals.push_back({2, 1});
+                    }
+                    break;
+                }
+ 
+                if ( x&1 ) x++;
+                else x = x >> 1;
+                proc++;
+            }
+        }
+        SORT(vals);
+        ll ans = INTMX , cnt = 0 , cur = -1 , sum = 0;
+ 
+        for ( auto i : vals ) {
+            if ( i.first != cur ) {
+                if ( cnt == n ) {
+                    ans = min (ans,sum);
+                }
+                    cur = i.first;
+                    cnt=1;
+                    sum = i.second;
+            }
+            else {
+                cnt++;
+                sum += i.second;
+            }
+        }
+        if ( cnt == n ) {
+            ans = min (ans,sum);
+        }
+        pans;
+    }
+}
